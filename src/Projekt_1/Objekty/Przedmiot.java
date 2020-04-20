3b116db1beb2544df39403ec231cc2eb0eaaf144
REDixum <alex.gudkow@yandex.ua>
2020-04-20 22:18:32 +0200

    help2

--- src/Projekt_1/Objekty/Przedmiot.java
+++ src/Projekt_1/Objekty/Przedmiot.java
@@ -1,12 +1,14 @@
 package Projekt_1.Objekty;
 
 public class Przedmiot extends Objekt {
+    private static int indeks = 0;
     public String nazwa;
     public int numerIndefikacyjnyPrzedmiotu;
 
     public Przedmiot(String nazwa, double szerokosc, double dlugosc, double wysokosc) {
         super(szerokosc, dlugosc, wysokosc);
         this.nazwa = nazwa;
+        numerIndefikacyjnyPrzedmiotu = indeks++;
     }
 
     public String toString() {
